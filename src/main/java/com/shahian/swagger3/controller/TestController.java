e00ab98319e2425c071cd96517f7d3163f8c3527
h.shahian <h.shahian@haytech.ir>
2023-06-26 10:03:38 +0330

    Initial commit

---
+++ src/main/java/com/shahian/swagger3/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.shahian.swagger3.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class TestController {
+    @GetMapping("v1/test")
+    public String getTest(){
+        return "these is a test api for swagger";
+    }
+}
